c2dbbe1369ff16584dd5ad18ecfa41d46099acf0
resteve <resteve@zikzakmedia.com>
2014-09-29 11:24:30 +0200

    pyflakes

--- product_oneclick.py
+++ product_oneclick.py
@@ -1,11 +1,9 @@
 # This file is part of product_oneclick_esale module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond.model import ModelView, fields
-from trytond.pool import Pool, PoolMeta
-from trytond.pyson import Eval, PYSONEncoder
-from trytond.wizard import Wizard, StateView, StateAction, StateTransition, \
-    Button
+from trytond.model import fields
+from trytond.pool import PoolMeta
+from trytond.pyson import Eval
 from trytond.modules.product_esale.tools import slugify
 
 __all__ = ['ProductOneClickView', 'ProductOneClick']
