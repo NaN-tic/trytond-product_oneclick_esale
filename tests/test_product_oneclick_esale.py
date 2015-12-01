961b979825242c5191f190d230a68c70f47863d0
resteve <resteve@zikzakmedia.com>
2015-12-01 11:13:35 +0100

    Tests

--- tests/test_product_oneclick_esale.py
+++ tests/test_product_oneclick_esale.py
@@ -1,32 +1,18 @@
-#!/usr/bin/env python
-# This file is part of product_oneclick_esale module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the product_oneclick_esale module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class ProductOneClickEsaleTestCase(unittest.TestCase):
-    'Test Product One Click eSale module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('product_oneclick_esale')
-
-    def test0005views(self):
-        'Test views'
-        test_view('product_oneclick_esale')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class ProductOneclickEsaleTestCase(ModuleTestCase):
+    'Test Product Oneclick Esale module'
+    module = 'product_oneclick_esale'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        ProductOneClickEsaleTestCase))
+        ProductOneclickEsaleTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
